c940f1e0424d868e3969b61d45bd869a5adfba65
2712 <2712@trustless.computer>
2025-12-05 11:53:44 +0700

    init

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -16,7 +16,7 @@ program
   .description(
     "Vibe Security - Find, verify and fix security vulnerabilities in your code"
   )
-  .version("1.0.2");
+  .version("1.0.3");
 
 // Security commands
 program
